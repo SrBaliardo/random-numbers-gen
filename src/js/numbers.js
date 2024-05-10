bd74d83cccaefb734a90fd87d6e0772320357ed4
SrBaliardo <tao.john007@icloud.com>
2024-05-10 09:47:29 -0300

    code update and fix - animation function and unique random number generator - v1.2

--- src/js/numbers.js
+++ src/js/numbers.js
@@ -6,25 +6,55 @@ const min = document.querySelector('.input-min')
 const max = document.querySelector('.input-max')
 const qtd = document.querySelector('.input-qtd')
 
-async function generateNumber() {
+function generateNumber() {
     const minN = Number(min.value)
     const maxN = Number(max.value)
     const qtdN = Number(qtd.value)
+    const numberDraw = new Set()
+
+    if (minN >= maxN) {
+        alert("O número mínimo tem que ser maior que o número maximo.");
+        return;
+    }
+
+    if ((maxN - minN + 1) < qtdN) {
+        alert("A quantidade de sorteios não pode exceder o intervalo de números");
+        return;
+    }
+
+    result.innerHTML = ''
+
+    for (let i = 0; i < qtdN; i++) {
+        let numberGen = Math.floor(Math.random() * (maxN - minN + 1) + minN)
+        while (numberDraw.has(numberGen)) {
+            numberGen = Math.floor(Math.random() * (maxN - minN + 1) + minN)
+        }
+
+        numberDraw.add(numberGen)
+            
+        const resultsP = document.createElement('div')
+        resultsP.classList.add('result')
+        resultsP.innerText = numberGen
+            
+        result.appendChild(resultsP)
+    }
+}
+
+async function toSpinDraw() {
+    const minN = Number(min.value)
+    const maxN = Number(max.value)
 
     for (let j = 0; j < 30; j++) {
         result.innerHTML = ''
+        let numberGen = Math.floor(Math.random() * (maxN - minN + 1) + minN)
 
-        for (let i = 0; i < qtdN; i++) {
-            const numberGen = Math.round(Math.random() * (maxN - minN + 1) + minN)
-            
-            const resultsP = document.createElement('div')
-            resultsP.classList.add('result')
+        const resultsP = document.createElement('div')
+        resultsP.classList.add('result')
             resultsP.innerText = numberGen
             
-            result.appendChild(resultsP)
-        }
+        result.appendChild(resultsP)
 
-        await wait(50)
+        await wait(100, generateNumber())
     }
 
     function wait(time) {
